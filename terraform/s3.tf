b8816b4c8de8fb96241be86a96393f74ca809dd5
SamipDave <118797360+SamipDave@users.noreply.github.com>
2024-12-17 05:12:25 +0000

    Update s3.tf

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -12,8 +12,8 @@ resource "aws_s3_bucket" "tf_s3_bucket" {
 resource "aws_s3_object" "tf_s3_object" {
   bucket   = aws_s3_bucket.tf_s3_bucket.bucket
   key      = "images/${each.key}" # each.key is the filename
-  source   = "nodejs-mysql/public/images/${each.key}"  # source directory
-  for_each = fileset("nodejs-mysql/public/images", "**") # get all files in the images directory
+  source   = "Nodejs_mysql_app_Terraform_project/public/images/${each.key}"  # source directory
+  for_each = fileset("Nodejs_mysql_app_Terraform_project/public/images/logo1.png", "**") # get all files in the images directory
 }
 
 # outputs
